1cd7593e9fc4a13536e2150b49d515ca3ba46668
duofan <2441051071@qq.com>
2023-08-30 22:11:49 +0800

    feat: 配置swagger引入knife

--- fly-web/fly-web-starter/src/main/java/com/duofan/fly/web/starter/FlyStarterApplication.java
+++ fly-web/fly-web-starter/src/main/java/com/duofan/fly/web/starter/FlyStarterApplication.java
@@ -1,17 +1,42 @@
 package com.duofan.fly.web.starter;
 
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Slf4j
 @ConfigurationPropertiesScan
 @SpringBootApplication
 public class FlyStarterApplication {
 
+    @SneakyThrows
     public static void main(String[] args) {
-        SpringApplication.run(FlyStarterApplication.class, args);
+        ConfigurableApplicationContext app = SpringApplication.run(FlyStarterApplication.class, args);
+
+        Environment env = app.getEnvironment();
+        log.info("""
+
+                        ----------------------------------------------------------
+                        \tApplication '{}' is running! Access URLs:
+                        \tLocal: \t\thttp://localhost:{}
+                        \tExternal: \thttp://{}:{}
+                        \tDoc: \thttp://{}:{}/doc.html
+                        ----------------------------------------------------------""",
+                env.getProperty("spring.application.name"),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"));
     }
 
 }
