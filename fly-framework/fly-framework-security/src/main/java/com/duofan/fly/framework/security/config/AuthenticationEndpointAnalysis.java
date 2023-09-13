ee57aa91b5193fe23fc025b2d785329fd1b29266
duofanHomePC <2441051071@qq.com>
2023-09-14 03:42:35 +0800

    fix: 基础搭建端点分析

---
+++ fly-framework/fly-framework-security/src/main/java/com/duofan/fly/framework/security/config/AuthenticationEndpointAnalysis.java
@@ -0,0 +1,62 @@
+package com.duofan.fly.framework.security.config;
+
+import com.duofan.fly.core.base.access.FlyAccessInfo;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * web controller 端点信息生成
+ *
+ * @author duofan
+ * @version 1.0
+ * @email 2441051071@qq.com
+ * @website duofan.top
+ * @date 2023/9/14
+ */
+@Slf4j
+@Component
+public class AuthenticationEndpointAnalysis implements CommandLineRunner {
+
+//PreAuthorizeAuthorizationManager
+
+    @Resource
+    private ApplicationContext applicationContext;
+
+    @PostConstruct
+    public void analysis() {
+
+
+//        AnnotationUtils.findAnnotation()
+
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("hello");
+        log.info("认证端点模块分析开始");
+        Map<String, Object> controllers = applicationContext.getBeansWithAnnotation(Controller.class);
+        for (Map.Entry<String, Object> entry : controllers.entrySet()) {
+            Object controller = entry.getValue();
+            Class<?> clazz = controller.getClass();
+            FlyAccessInfo annotation = AnnotationUtils.findAnnotation(clazz, FlyAccessInfo.class);
+            if (annotation == null) continue;
+            System.out.println(annotation.system());
+            Method[] controllerMethods = controller.getClass().getDeclaredMethods();
+            List<Method> flyMethods = Arrays.stream(controllerMethods).filter(m -> m.isAnnotationPresent(FlyAccessInfo.class)).toList();
+            for (Method flyMethod : flyMethods) {
+                System.out.println(flyMethod.getName());
+            }
+        }
+    }
+}
--- fly-framework/fly-framework-security/src/main/java/com/duofan/fly/framework/security/config/SecurityConfig.java
+++ fly-framework/fly-framework-security/src/main/java/com/duofan/fly/framework/security/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -37,7 +38,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().authenticated()
                 )
-
+                .httpBasic(Customizer.withDefaults())
         ;
         return http.build();
     }
