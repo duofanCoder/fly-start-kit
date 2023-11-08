43d1eea6623c3b603c5e83b581eaee4b4b845191
duofanHomePC <2441051071@qq.com>
2023-11-08 23:21:51 +0800

    feat: 文件上传模块添加

---
+++ fly-api/fly-api-file/src/main/java/com/duofan/fly/api/file/controller/api/v1/FlyFileController.java
@@ -0,0 +1,14 @@
+package com.duofan.fly.api.file.controller.api.v1;
+
+/**
+ * 文件接口
+ *
+ * @author duofan
+ * @version 1.0
+ * @email 2441051071@qq.com
+ * @website duofan.top
+ * @date 2023/11/8
+ */
+
+public class FlyFileController {
+}
