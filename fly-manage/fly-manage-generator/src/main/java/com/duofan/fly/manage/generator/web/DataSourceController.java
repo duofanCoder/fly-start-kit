5b241590f9b19b786d92ad342fd9fe89c574981d
duofan-dell <duofancc@gmail.com>
2023-11-17 13:07:41 +0800

    feat: 代码生成模块初始化

--- fly-manage/fly-manage-generator/src/main/java/com/duofan/fly/manage/generator/web/DataSourceController.java
+++ fly-manage/fly-manage-generator/src/main/java/com/duofan/fly/manage/generator/web/DataSourceController.java
@@ -1,26 +1,19 @@
 package com.duofan.fly.manage.generator.web;
 
 import com.duofan.fly.core.base.domain.permission.access.FlyAccessInfo;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/gen")
+@RequestMapping("/api/v1/datasource")
 @FlyAccessInfo(system = "FLY BOOT")
-public class TemplateConfigController {
-
+public class DataSourceController {
     @GetMapping("/")
     @FlyAccessInfo(module = "模板配置", op = "查询", description = "查询模板配置列表", needAuthenticated = false)
     String index() {
         return "index";
     }
 
-    @GetMapping("list")
-    @FlyAccessInfo(module = "模板1配置", op = "查询", description = "查询模板配置列表", needAuthenticated = false)
-    String list() {
-        return "list";
-    }
 
 }
